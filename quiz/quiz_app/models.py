ab1fbae3e6eb0099ad3cf1b18095b18ebd42ff63
Daniel <danielmichalik98@wp.pl>
2020-07-24 22:54:42 +0200

    Add models and create obj in data base

--- quiz/quiz_app/models.py
+++ quiz/quiz_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=300)
+    publication_date = models.DateField()
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=100, blank=True)
+    answer_int = models.IntegerField(blank=True)
+
+    def __str__(self):
+        return self.answer_text
