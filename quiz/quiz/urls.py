412e9ceabe461e5d60bcc78b38371b2547e6c6e7
Daniel <danielmichalik98@wp.pl>
2020-08-14 19:38:15 +0200

    Add registration page

--- quiz/quiz/urls.py
+++ quiz/quiz/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from accounts.views import home
 
 urlpatterns = [
     path('quiz_app/', include('quiz_app.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('', home, name="home"),
     path('admin/', admin.site.urls),
 ]
