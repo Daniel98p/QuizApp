c953148f2005235439b5a044c991226292ecab6e
Daniel <danielmichalik98@wp.pl>
2020-08-14 20:42:49 +0200

    Add login page

--- quiz/quiz/urls.py
+++ quiz/quiz/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from accounts.views import home
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('quiz_app/', include('quiz_app.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
-    path('', home, name="home"),
+    path('', TemplateView.as_view(template_name='accounts/home.html'), name='home'),
     path('admin/', admin.site.urls),
 ]
