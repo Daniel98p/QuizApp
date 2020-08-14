4eea59d68d1d2d517a016d127017c9518ccb3cc1
Daniel <danielmichalik98@wp.pl>
2020-08-14 19:38:42 +0200

    Add registration page

---
+++ quiz/accounts/views.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+
+def home(request):
+    return render(request, 'accounts/home.html')
+
+
+def signup(request):
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect('home')
+    return render(request, 'accounts/signup.html', {'form': form})
