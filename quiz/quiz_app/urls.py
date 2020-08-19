e6c1ac8c39edd000409a4d128360e3ec7e4440dd
Daniel <danielmichalik98@wp.pl>
2020-08-19 16:28:25 +0200

    Add next quizzes

--- quiz/quiz_app/urls.py
+++ quiz/quiz_app/urls.py
@@ -4,6 +4,6 @@ from .views import select_quiz, render_quiz, show_results
 app_name = "quiz_app"
 urlpatterns = [
     path('', select_quiz, name='select-quiz'),
-    path('1/', render_quiz, name="render-quiz"),
-    path('1/results/', show_results, name='show-results'),
+    path('<int:quiz_id>/', render_quiz, name="render-quiz"),
+    path('<int:quiz_id>/results/', show_results, name='show-results'),
               ]
--- quiz/quiz_app/views.py
+++ quiz/quiz_app/views.py
@@ -1,37 +1,39 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Question, Answer
+from .models import Question, Answer, Quiz
 from .forms import QuizForm
 
 
 @login_required
 def select_quiz(request):
-    question = Question.objects.last()
-    context = {"question": question}
+    quizzes = Quiz.objects.all()
+    context = {"quizzes": quizzes}
     return render(request, 'quiz_app/selector.html', context=context)
 
 
 @login_required
-def render_quiz(request):
-    questions = Question.objects.all()
-    answers = Answer.objects.all()
+def render_quiz(request, quiz_id):
+    questions = Question.objects.filter(quiz_id=quiz_id)
+    quiz = Quiz.objects.get(pk=quiz_id)
+    questions_id = [question.id for question in questions]
+    answers = Answer.objects.filter(question_id__in=questions_id)
     if request.method == 'POST':
         form = QuizForm(request.POST, answers=answers, questions=questions)
         if form.is_valid():
             request.session["question_1"] = form.cleaned_data["question_1"]
             request.session["question_2"] = form.cleaned_data["question_2"]
             request.session["question_3"] = form.cleaned_data["question_3"]
-            return redirect('/quiz_app/1/results/')
+            return redirect('quiz_app:show-results', quiz_id=quiz_id)
     else:
         form = QuizForm(answers=answers, questions=questions)
-    context = {"questions": questions, "form": form}
+    context = {"questions": questions, "form": form, "quiz": quiz}
     return render(request, "quiz_app/index.html", context=context)
 
 
 @login_required
-def show_results(request):
-    questions = Question.objects.all()
+def show_results(request, quiz_id):
+    questions = Question.objects.filter(quiz_id=quiz_id)
     questions_id = [question.id for question in questions]
     try:
         selected_choice_1 = request.session.get('question_1')
@@ -44,6 +46,8 @@ def show_results(request):
     selected_choice_obj_3 = Answer.objects.get(answer_text=selected_choice_3)
     selected_choices_question_id = [selected_choice_obj_1.question_id, selected_choice_obj_2.question_id,
                                     selected_choice_obj_3.question_id]
+    print(questions_id)
+    print(selected_choice_obj_1.question_id)
     good_answers = 0
     for question, choice in zip(questions_id, selected_choices_question_id):
         if question == choice:
